f8d9817e9ec43f7823b48d7d75f9341b8cd0ed3e
anas <anas_khrisat@hotmail.com>
2022-09-18 17:01:19 -0600

    Add files

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,10 +1,14 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import '../../App.css';
 
 export default function services() {
-  return (
-<>
-<h1 className='services'> SERVICES</h1>;
-</>
-  );
+  render();{ 
+    return (
+      <>
+      <h1 className='services'> SERVICES</h1>;
+      </>
+        );
+  }
+  
 }
